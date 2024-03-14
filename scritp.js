f5059aac42f945d09e2a138db4e09d83b649cc5c
Guilherme-marquesss <carvalhogui.marques@gmail.com>
2024-03-14 11:25:42 -0300

    criação da navegação desktop, remocação da classe header-mobile e inicio da responsividade

--- scritp.js
+++ scritp.js
@@ -1,15 +1,15 @@
-const botaoAbrir = document.querySelector(".header-mobile>button")
-const botaoFechar = document.querySelector(".header-mobile nav button")
+const botaoAbrir = document.querySelector("header>button")
+const botaoFechar = document.querySelector("header .nav .button")
 
 const body = document.querySelector("body")
-const nav = document.querySelector(".header-mobile nav")
+const nav = document.querySelector("header .mobile")
 botaoAbrir.addEventListener('click', abrirmenu)
 botaoFechar.addEventListener('click', fecharmenu)
-function fecharmenu(){
-    body.classList.remove("escurecer")
-    nav.classList.remove("abrir")
-}
 function abrirmenu() {
     body.classList.add("escurecer")
     nav.classList.add("abrir")
+}
+function fecharmenu(){
+    body.classList.remove("escurecer")
+    nav.classList.remove("abrir")
 }
