c3c4d43b391cc1626f71d85d6b9a89df860c3a7b
Guilherme-marquesss <carvalhogui.marques@gmail.com>
2024-03-14 11:28:11 -0300

    adição dos hover, estilização da terceira seção e termino de projeto

--- scritp.js
+++ scritp.js
@@ -1,15 +1,16 @@
 const botaoAbrir = document.querySelector("header>button")
 const botaoFechar = document.querySelector("header .nav .button")
-
 const body = document.querySelector("body")
 const nav = document.querySelector("header .mobile")
 botaoAbrir.addEventListener('click', abrirmenu)
 botaoFechar.addEventListener('click', fecharmenu)
+
 function abrirmenu() {
-    body.classList.add("escurecer")
-    nav.classList.add("abrir")
+    body.classList.add("escurecer");
+    nav.classList.add("abrir");
 }
-function fecharmenu(){
-    body.classList.remove("escurecer")
-    nav.classList.remove("abrir")
+
+function fecharmenu() {
+    body.classList.remove("escurecer");
+    nav.classList.remove("abrir");
 }
